e0179a266966af54171714c793e7b270c97df50a
Kirill Lapidus <kirill.lapidus@gmail.com>
2023-12-11 16:12:27 +0100

    implement insertR

---
+++ insertr.scm
@@ -0,0 +1,9 @@
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote()))
+      (else (cond
+              ((eq? (car lat) old)
+               (cons old (cons new (cdr lat))))
+              (else (cons (car lat) (insertR new old
+                                      (cdr lat)))))))))
